1c34c068b6a4d615a7dbfc41ed61421a9f67ef22
William Edwards <williamedwards7717@gmail.com>
2023-03-22 09:04:37 -0600

    houses done

--- server/controllers/HousesController.js
+++ server/controllers/HousesController.js
@@ -7,7 +7,10 @@ export class HousesController extends BaseController {
         super('api/houses')
         this.router
             .get('', this.getHouses)
+            .get('/:houseId', this.getHouseById)
             .post('', this.createHouse)
+            .delete('/:houseId', this.deleteHouse)
+            .put('/:houseId', this.editHouse)
     }
 
     async getHouses(req, res, next) {
@@ -20,11 +23,42 @@ export class HousesController extends BaseController {
         }
     }
 
+    async getHouseById(req, res, next) {
+        try {
+            const houseId = req.params.houseId
+            const house = await housesService.getHouseById(houseId)
+            res.send(house)
+        } catch (error) {
+            next(error)
+        }
+    }
+
     async createHouse(req, res, next) {
         try {
             const houseData = req.body
             const newHouse = await housesService.createHouse(houseData)
             res.send(newHouse)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async deleteHouse(req, res, next) {
+        try {
+            const houseId = req.params.houseId
+            await housesService.deleteHouse(houseId)
+            res.send('deleted house')
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async editHouse(req, res, next) {
+        try {
+            const houseEdits = req.body
+            const houseId = req.params.houseId
+            const editedHouse = await housesService.editHouse(houseEdits, houseId)
+            res.send(editedHouse)
         } catch (error) {
             next(error)
         }
--- server/services/HousesService.js
+++ server/services/HousesService.js
@@ -1,4 +1,5 @@
 import { dbContext } from "../db/DbContext.js"
+import { BadRequest } from "@bcwdev/auth0provider/lib/Errors.js"
 
 
 
@@ -10,9 +11,38 @@ class HousesService {
         return houses
     }
 
+    async getHouseById(houseId) {
+        const house = await dbContext.Houses.findById(houseId)
+        if (!house) {
+            throw new BadRequest('House id invalid')
+        }
+        return house
+    }
+
     async createHouse(houseData) {
         const newHouse = dbContext.Houses.create(houseData)
         return newHouse
+    }
+
+    async deleteHouse(houseId) {
+        const house = await dbContext.Houses.findByIdAndDelete(houseId)
+        if (!house) {
+            throw new BadRequest('No house with that id')
+        }
+        return
+    }
+
+    async editHouse(houseEdits, houseId) {
+        const originalHouse = await this.getHouseById(houseId)
+        originalHouse.bedroom = houseEdits.bedroom ? houseEdits.bedroom : originalHouse.bedroom
+        originalHouse.bathroom = houseEdits.bathroom || originalHouse.bathroom
+        originalHouse.level = houseEdits.level ? houseEdits.level : originalHouse.level
+        originalHouse.furnace = houseEdits.furnace ? houseEdits.furnace : originalHouse.furnace
+        originalHouse.description = houseEdits.description ? houseEdits.description : originalHouse.description
+        originalHouse.hasBasement = houseEdits.hasBasement != null ? houseEdits.hasBasement : originalHouse.hasBasement
+
+        await originalHouse.save()
+        return originalHouse
     }
 
 }
